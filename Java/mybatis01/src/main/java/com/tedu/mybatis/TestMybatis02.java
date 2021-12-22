77588490b9edc519e2b935b7c15497d16fc9bc53
赵灵儿 <lautsinghua@163.com>
2021-12-22 11:29:02 +0800

    更新

--- Java/mybatis01/src/main/java/com/tedu/mybatis/TestMybatis02.java
+++ Java/mybatis01/src/main/java/com/tedu/mybatis/TestMybatis02.java
@@ -7,6 +7,9 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * mybatis的增删改查和占位符#{}${}
@@ -23,6 +26,14 @@ public class TestMybatis02 {
          session = factory.openSession(true);
         } catch (Exception exception) {
             exception.printStackTrace();
+        }
+
+    }
+    @Test
+    public void testFindAll() {
+        List<Emp> list = session.selectList("EmpMapper.findAll");
+        for (Emp emp:list) {
+            System.out.println(emp);
         }
 
     }
@@ -49,14 +60,67 @@ public class TestMybatis02 {
 
     }
 
-    /*===================占位符练习=======================**/
-    /**新增员工信息:刘德华 歌手 8888*/
+    /**=============================================占位符练习================================================**/
+    /**查找对应ID的员工信息*/
     @Test
     public void testFindById(){
         Integer id=1;
         Emp emp = session.selectOne("EmpMapper.findById",id);//selectOne("定位标识",参数);
         System.out.println(emp);
 
+
+    }
+    /**新增员工信息:用Map进行封装*/
+    @Test
+    public void testInsert2(){
+       //将参数封装到一个map中
+        Map<String,Object> map=new HashMap<>();
+        map.put("name", "张飞");
+        map.put("job","Java开发工程师");
+        map.put("salary",15000);
+        int i = session.update("EmpMapper.insert2", map);//update("定位标识", 参数名);
+        System.out.println(i+"行受到了影响");
+
+
+    }
+    /**新增员工信息:用pojo进行封装*/
+    @Test
+    public void testInsert3(){
+       Emp emp =new Emp();
+       emp.setName("关羽");
+       emp.setJob("保安");
+       emp.setSalary(8000d);
+        int i = session.update("EmpMapper.insert3", emp);
+        System.out.println(i+"行受到了影响");
+    }
+    /**修改员工信息:关羽 为程序员 薪资为 3400*/
+    @Test
+    public void testUpdate2(){
+        Map<String,Object> map=new HashMap<>();
+        map.put("name", "关羽");
+        map.put("job","程序员");
+        map.put("salary",3400);
+        int rows = session.update("EmpMapper.update2",map);
+        System.out.println(rows+"行修改成功!");
+
+    }
+    /**修改员工信息:用pojo进行封装*/
+    @Test
+    public void testupdate3(){
+        Emp emp =new Emp();
+        emp.setName("刘德华");
+        emp.setJob("保安");
+        emp.setSalary(8000d);
+        int i = session.update("EmpMapper.update3", emp);
+        System.out.println(i+"行受到了影响");
+    }
+
+    /**删除员工信息:刘德华 */
+    @Test
+    public void testDelete2(){
+        Integer id=16;
+        int rows = session.update("EmpMapper.delete2",id);
+        System.out.println(rows+"行删除成功!");
 
     }
 }
