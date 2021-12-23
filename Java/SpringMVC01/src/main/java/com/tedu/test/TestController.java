cff5452e1b7e6d10914f60f0533c73061e89c2d8
赵灵儿 <lautsinghua@163.com>
2021-12-23 22:40:22 +0800

    22:33

---
+++ Java/SpringMVC01/src/main/java/com/tedu/test/TestController.java
@@ -0,0 +1,12 @@
+package com.tedu.test;
+
+
+import org.junit.Test;
+
+public class TestController {
+    @Test
+    public void testController(){
+
+    }
+
+}
--- Java/spring01/src/main/java/com/tedu/dao/EmpDao.java
+++
@@ -1,8 +0,0 @@
-package com.tedu.dao;
-/**
- * 员工模块的Dao(持久层)接口
- */
-public interface EmpDao {
-	/** 添加员工信息 */
-	public void addEmp();
-}
